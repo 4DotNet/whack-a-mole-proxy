260508f3b6685efaf7b02e4da5f67df4575f4a58
Eduard Keilholz <e.keilholz@4dotnet.nl>
2024-01-17 16:29:20 +0100

    Without remove config

--- src/Wam.Proxy.Api/Program.cs
+++ src/Wam.Proxy.Api/Program.cs
@@ -7,24 +7,24 @@ using Yarp.ReverseProxy.Configuration;
 var corsPolicyName = "DefaultCors";
 var builder = WebApplication.CreateBuilder(args);
 
-var azureCredential = CloudIdentity.GetCloudIdentity();
-try
-{
-    builder.Configuration.AddAzureAppConfiguration(options =>
-    {
-        var appConfigurationUrl = builder.Configuration.GetRequiredValue("AzureAppConfiguration");
-        options.Connect(new Uri(appConfigurationUrl), azureCredential)
-            .UseFeatureFlags();
-    });
-}
-catch (Exception ex)
-{
-    throw new Exception("Failed to configure the Whack-A-Mole Realtime service, Azure App Configuration failed", ex);
-}
+//var azureCredential = CloudIdentity.GetCloudIdentity();
+//try
+//{
+//    builder.Configuration.AddAzureAppConfiguration(options =>
+//    {
+//        var appConfigurationUrl = builder.Configuration.GetRequiredValue("AzureAppConfiguration");
+//        options.Connect(new Uri(appConfigurationUrl), azureCredential)
+//            .UseFeatureFlags();
+//    });
+//}
+//catch (Exception ex)
+//{
+//    throw new Exception("Failed to configure the Whack-A-Mole Realtime service, Azure App Configuration failed", ex);
+//}
 
 builder.Services.AddHealthChecks();
-builder.Services.AddApplicationInsightsTelemetry();
-builder.Services.AddServiceConfiguration(builder.Configuration);
+//builder.Services.AddApplicationInsightsTelemetry();
+//builder.Services.AddServiceConfiguration(builder.Configuration);
 builder.Services
     .AddSingleton<IProxyConfigProvider, CustomProxyConfigProvider>()
     .AddReverseProxy();
